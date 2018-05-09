a0ff9badc046a56cfc03594b761b1bf96ff3c280
Erik Ruhmanis <erik2310@edu.easj.dk>
2018-05-09 14:14:53 +0200

    Initial commit

---
+++ app/src/main/java/com/jfdimarzio/notifications/MainActivity.java
@@ -0,0 +1,37 @@
+package com.jfdimarzio.notifications;
+
+import android.app.Activity;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v4.app.NotificationCompat;
+import android.view.View;
+
+public class MainActivity extends Activity {
+    int notificationID = 1;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+    public void onClick(View view) {
+        displayNotification();
+    }
+
+    protected void displayNotification()
+    {
+        //---PendingIntent to launch activity if the user selects
+        // this notification---
+        Intent i = new Intent(this, NotificationView.class);
+        i.putExtra("notificationID", notificationID);
+        PendingIntent pendingIntent = PendingIntent.getActivity(this, 0, i, 0);
+        NotificationManager nm = (NotificationManager)getSystemService(NOTIFICATION_SERVICE);
+        NotificationCompat.Builder notifBuilder;
+        notifBuilder = new NotificationCompat.Builder(this)
+                .setSmallIcon(R.mipmap.ic_launcher)
+                .setContentTitle("Meteor på vej mod jorden!")
+                .setContentText("En meteor rammer jorden om 5 minutter!");
+        nm.notify(notificationID, notifBuilder.build());
+    }
+}
